297ba640ce77709c787d362cdd45cd4be5476cd4
jfouret <julien@fouret.me>
2024-06-03 10:53:28 +0200

    import pipeline and pipeline OK

--- yanfui/utils/git.py
+++ yanfui/utils/git.py
@@ -4,8 +4,46 @@ from git import Repo
 import shutil
 import tempfile
 import json
+import yaml
 from pathlib import Path
 import time
+import re
+# Replace the matched pattern with a colon
+
+def parse_nf_manifest(file_path):
+    manifest_detected = False
+    open_brackets = 0
+    closed_brackets = 0
+    i=0
+    description = ""
+    nextflowVersion = ""
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            i += 1
+            if line.startswith('manifest'):
+                manifest_detected = True
+            if manifest_detected:
+                open_brackets += line.count('{')
+                closed_brackets += line.count('}')
+            else:
+                continue
+            if open_brackets == closed_brackets:
+                break
+            match = re.match(r"^\s*description\s*=\s*['\"]+([^'\"].*[^'\"])['\"]+\s*$", line)
+            if match:
+                description = match.group(1)
+                continue
+            match = re.match(r"^\s*nextflowVersion\s*=\s*['\"](.*)['\"]\s*$", line)
+            if match:
+                nextflowVersion = match.group(1)
+                continue
+            if open_brackets < closed_brackets:
+                raise Exception(f'Invalid manifest file "{file_paht}", closing brackets without opening line {i}')
+    results = {
+        'description': description,
+        'nextflowVersion': nextflowVersion
+    }
+    return results
 
 def run_command(command, directory):
     # Use subprocess.run to execute the command
@@ -30,22 +68,22 @@ def fetch_repo_details(organization,project,base_target_path, force = False ,bas
 
     cache_path = f'{base_target_path}/{organization}/{project}.json'
     current_epoch = int(time.time())
-    # if file eexists
+    # if file exists
     if os.path.exists(cache_path) and not force:
         with open(cache_path, 'r') as f:
             data = json.load(f)
         epoch = int(data["time"])
-        if (current_epoch - epoch) > 300:
+        if (current_epoch - epoch) < 300:
             return data["results"]
 
     repo_url = build_url(organization, project, base_url)
+    nf_config_path = f'{base_target_path}/{organization}/{project}/nextflow.config'
+    results = parse_nf_manifest(nf_config_path)
     with tempfile.TemporaryDirectory() as tmpdirname:
         # Clone the repository as a bare repository
         repo = Repo.clone_from(repo_url, tmpdirname, bare=True)
-        results = {
-            'tags': [ x.name for x in repo.tags],
-            'branches': [x.name for x in repo.branches]
-        }
+        results['tags'] = [ x.name for x in repo.tags]
+        results['branches'] = [ x.name for x in repo.branches]
     try:
         repo_obj = Repo(f'{base_target_path}/{organization}/{project}')
         try:
